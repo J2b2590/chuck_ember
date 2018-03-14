b935a549f57fce07cb15549845d4d1cd991bdae1
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2018-03-14 16:08:32 -0500

    api is called for jokes

--- app/components/chuck-api.js
+++ app/components/chuck-api.js
@@ -1,4 +1,28 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	gotJoke: false,
+	joke: '',
+	actions:{
+		getJoke: function(){
+			var that = this;
+			$.ajax({
+				type: 'GET',
+				url: 'https://api.chucknorris.io/jokes/random',
+				success: function(response){
+					console.log(response.value)
+					that.setProperties({
+						joke: response.value,
+						gotJoke: true
+						
+					});
+
+				},
+				error: function() { 
+					alert("Mr.Norris has no jokes for you");
+				}
+			});
+		}
+
+	}
 });
